89246446583d07f48d2a59c0c445c39da8549234
Sanbedan Paul <sanbedan9989@gmail.com>
2023-08-18 02:37:13 +0530

    alander added successfully

--- client/src/components/Others/Calander/Day.jsx
+++ client/src/components/Others/Calander/Day.jsx
@@ -1,25 +1,35 @@
 import React from "react";
+import "./Day.css";
 
 class Day extends React.Component {
   render() {
     const { date, notice, onApprove, onDecline } = this.props;
+    const day = date.getDate();
+    const dayOfWeek = date.toLocaleString("en-US", { weekday: "short" });
 
     return (
-      <div className={`day${notice ? " notice" : ""}`}>
-        <div className="date">{date.getDate()}</div>
-        {notice && (
-          <div className="notice-box">
-            <div className="notice-text">{notice.notice}</div>
-            <div className="notice-actions">
-              <span className="approve" onClick={onApprove}>
-                ✔️
-              </span>
-              <span className="decline" onClick={onDecline}>
-                ❌
-              </span>
-            </div>
-          </div>
+      <div className="day">
+        <div className="date">{day}</div>
+        {notice ? (
+          <div className="notice-dot"></div>
+        ) : (
+          <div className="day-of-week">{dayOfWeek}</div>
         )}
+        <div className="notice-box">
+          {notice && (
+            <React.Fragment>
+              <p>{notice.notice}</p>
+              <div className="notice-actions">
+                <button className="approve" onClick={() => onApprove(notice)}>
+                  ✔️
+                </button>
+                <button className="decline" onClick={() => onDecline(notice)}>
+                  ❌
+                </button>
+              </div>
+            </React.Fragment>
+          )}
+        </div>
       </div>
     );
   }
